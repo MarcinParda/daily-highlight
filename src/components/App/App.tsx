17b5fdbd2a8857b31deb32a2b9e0828e01abbb72
MarcinParda <marcin98parda@gmail.com>
2022-05-23 23:39:58 +0200

    fetch data from localhost:5000

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+
 export function App() {
+  const { data } = useQuery("secret", () =>
+    axios.get("https://localhost:5000/secret").then((res) => res.data)
+  );
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello Frontend</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
+import queryClient from "./api/queryClient";
 import { App } from "./components/App";
 import "./index.css";
 
@@ -8,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
